34779b7067087ee0a75332f8020371e7df67c567
Madeline Stark <madeline.stark1@gmail.com>
2018-07-18 20:29:06 +0000

    automatically backed up by learn

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -1,12 +1,10 @@
 class CLI
-  attr_accessor :player_uno, :player_dos, :game
+  attr_accessor :game
 
 
   def initialize
-    @game = game
+    #@game = game
     start
-    @player_uno = player_uno
-    @player_dos = player_dos
   end
 
   def start
@@ -15,11 +13,11 @@ class CLI
     input = gets.strip.to_i
 
     if input == 0
-      game = computer_vs_computer
+      computer_vs_computer
     elsif input == 1
-      game = computer_vs_human
+      computer_vs_human
     elsif input == 2
-      game = human_vs_human
+      human_vs_human
     end
     game.play #can call lower case because we defined it up there
   end
@@ -28,9 +26,9 @@ class CLI
     puts "Which Computer should go first? 1 or 2?"
     answer = gets.strip.to_i
     if answer == 1
-      game = Game.new(player_uno = Players::Computer.new("X"), player_dos = Players::Computer.new("O"))
+      @game = Game.new(player_uno = Players::Computer.new("X"), player_dos = Players::Computer.new("O"))
     else
-      game = Game.new(player_uno = Players::Computer.new("O"), player_dos = Players::Computer.new("X"))
+      @game = Game.new(player_uno = Players::Computer.new("O"), player_dos = Players::Computer.new("X"))
     end
   end
 
@@ -38,9 +36,9 @@ class CLI
     puts "Which should go first? Computer or human?"
     answer = gets.strip.to_i
     if answer == "Computer"
-      game = Game.new(player_uno = Players::Computer.new("X"), player_dos = Players::Human.new("O"))
+      @game = Game.new(player_uno = Players::Computer.new("X"), player_dos = Players::Human.new("O"))
     else
-      game = Game.new(player_uno =Players::Human.new("O"), player_dos = Players::Computer.new("X"))
+      @game = Game.new(player_uno =Players::Human.new("O"), player_dos = Players::Computer.new("X"))
     end
   end
 
@@ -48,25 +46,12 @@ class CLI
     puts "Which player should go first? 1 or 2?"
     answer = gets.strip.to_i
     if answer == 1
-      game = Game.new(player_uno = Players::Human.new("X"), player_dos = Players::Human.new("O"))
+      @game = Game.new(player_uno = Players::Human.new("X"), player_dos = Players::Human.new("O"))
     else
-      game = Game.new(player_uno = Players::Human.new("O"), player_dos = Players::Human.new("X"))
+      @game = Game.new(player_uno = Players::Human.new("O"), player_dos = Players::Human.new("X"))
     end
   end
 
-  def finished
-    binding.pry
-    if game.over?
-      puts "Would you like to play again? Y/n"
-      answer = gets.strip
-    end
-    if answer == "Y"
-      game = CLI.new
-    else
-      puts "Later!"
-      exit
-    end
-  end
 
 
 end
