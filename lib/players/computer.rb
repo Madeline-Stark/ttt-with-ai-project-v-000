13dea6e7732f7fd3e4d962736f6142427b1feed9
Madeline Stark <madeline.stark1@gmail.com>
2018-07-18 19:31:23 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,7 +3,7 @@ module Players
     attr_accessor :board
 
     def move(board)
-      @board = board
+      #@board = board
       if !board.taken?(5)
         choice = 5
       elsif !board.taken?(1)
@@ -14,21 +14,27 @@ module Players
         choice = 7
       elsif !board.taken?(9)
         choice = 9
-      else
-        try_again
+      elsif !board.taken?(2)
+        choice = 2
+      elsif !board.taken?(4)
+        choice = 4
+      elsif !board.taken?(6)
+        choice = 6
+      elsif !board.taken?(8) 
+        choice
       end
       "#{choice}"
     end
 
-    def try_again
-      #options = [2, 4, 6, 8]
-      if !board.taken?(choice = rand(2..8)) #(choice = rand(options))
-        choice
-      else
-        #options.remove(choice)
-        try_again
-      end
-    end
+    # def try_again
+    #   #options = [2, 4, 6, 8]
+    #   if !board.taken?(choice = rand(2..8)) #(choice = rand(options))
+    #     choice
+    #   else
+    #     #options.remove(choice)
+    #     try_again
+    #   end
+    # end
 
   end
 end
