ca9e9ab62042b36c265ecd5caf95fab6ee74865c
Madeline Stark <madeline.stark1@gmail.com>
2018-07-18 19:22:06 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,7 +1,9 @@
 module Players
   class Computer < Player
+    attr_accessor :board
 
     def move(board)
+      @board = board 
       if !board.taken?(5)
         choice = 5
       elsif !board.taken?(1)
