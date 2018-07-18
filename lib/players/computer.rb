153492852832cd6eb27efda8664c1a75a19507e4
Madeline Stark <madeline.stark1@gmail.com>
2018-07-18 19:16:56 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,28 +4,27 @@ module Players
     def move(board)
       if !board.taken?(5)
         choice = 5
-      elsif !board.taken?(1) 
+      elsif !board.taken?(1)
         choice = 1
-      elsif !board.taken?(3) 
+      elsif !board.taken?(3)
         choice = 3
-      elsif !board.taken?(7) 
+      elsif !board.taken?(7)
         choice = 7
-      elsif !board.taken?(9) 
+      elsif !board.taken?(9)
         choice = 9
-      else 
+      else
         try_again
-      end 
-      "#{choice}" 
+      end
+      "#{choice}"
     end
 
     def try_again
-      if !board.taken?(choice = rand(2,4,6,8)) 
-        choice 
+      if !board.taken?(choice = rand(2,4,6,8))
+        choice
       else
         try_again
-      end 
-    end 
+      end
+    end
 
   end
 end
-
